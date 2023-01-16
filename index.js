7b64193808c77987fa50da41b4e278b4242c7d6e
Tess <tesstelier@gmail.com>
2023-01-16 12:15:56 -0800

    Switch from standard to google style guide, fix accompanying issues

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const connect = require('connect');
 const connectLivereload = require('connect-livereload');
 const gulpLivereload = require('gulp-livereload');
-const { watch } = require('gulp');
+const {watch} = require('gulp');
 const serveStatic = require('serve-static');
 
 const defaultOptions = {
-    livereloadPort: 35729,
-    localhostPort: 8080
+  livereloadPort: 35729,
+  localhostPort: 8080,
 };
 
+/**
+ * Start connect server
+ *
+ * @param {*} config
+ */
 function initServer(config) {
-    connect()
-        .use(connectLivereload({ port: config.livereloadPort }))
-        .use(serveStatic(config.rootDir))
-        .listen(config.localhostPort);
+  connect()
+      .use(connectLivereload({port: config.livereloadPort}))
+      .use(serveStatic(config.rootDir))
+      .listen(config.localhostPort);
 }
 
+/**
+ * Start LiveReload and watching for files changes
+ *
+ * @param {*} config
+ */
 function livereload(config) {
-    gulpLivereload.listen({ port: config.livereloadPort });
+  gulpLivereload.listen({port: config.livereloadPort});
 
-    const watcher = watch(config.watchGlob);
-    watcher.on('change', function onChange(path) {
-        gulpLivereload.changed(path); // Simply calling gulpLivereload() doesn't work
-    });
-    /* Alternatively (con: reloads whole page regardless of which file changed):
+  const watcher = watch(config.watchGlob);
+  watcher.on('change', function onChange(path) {
+    gulpLivereload.changed(path); // Just calling gulpLivereload() doesn't work
+  });
+  /* Alternatively (con: reloads whole page regardless of which file changed):
     watch(config.watchGlob, function onChange(cb) {
         gulpLivereload.reload(); // Simply calling gulpLivereload() doesn't work
         cb();
@@ -31,13 +41,18 @@ function livereload(config) {
     */
 }
 
+/**
+ * Gulp task to start LiveReload
+ *
+ * @param {*} options See options in README
+ */
 function run(options) {
-    const config = Object.assign(
-        Object.assign({}, defaultOptions),
-        options
-    );
-    initServer(config);
-    livereload(config);
+  const config = Object.assign(
+      Object.assign({}, defaultOptions),
+      options,
+  );
+  initServer(config);
+  livereload(config);
 }
 
 module.exports = run;
